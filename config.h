f8bbc645a21e09481fa96ad17381a1b50ab39738
Arch Abudin <rifqifadil57@gmail.com>
2025-05-14 19:58:03 +0700

    up sebelum pindah ke alpine linuj

--- config.h
+++ config.h
@@ -3,21 +3,21 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int default_border = 0;   /* to switch back to default border after dynamic border resizing via keybinds */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int snap      = 5;       /* snap pixel */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails,display systray on the 1st monitor,False: display systray on last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int showtab            = showtab_auto;
+static const int showbar            = 0;        /* 0 means no bar */
 static const int toptab             = 1;        /* 0 means bottom tab */
+static const int showtab            = showtab_auto;
 static const int floatbar           = 1;/* 1 means the bar will float(don't have padding),0 means the bar have padding */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 5;
@@ -41,12 +41,12 @@ static const int new_window_attach_on_end = 0; /*  1 means the new window will a
 #define ICONSIZE 19   /* icon size */
 #define ICONSPACING 8 /* space between icon and title */
 
-static const char *fonts[]          = {"Monocraft Nerd Font:style:Light:size=12","Iosevka:style:medium:size=12" ,"JetBrainsMono Nerd Font Mono:style:medium:size=12"," ComicCodeLigaturesNerdFontComplete Nerd Font:style=Regular:size=12", "Noto Sans CJK JP:style=Regular:size=12", "Noto Sans JP Medium:style=Mediun:size=12" };
+static const char *fonts[]          = {"Monocraft Nerd Font:style:Light:size=10","Iosevka:style:medium:size=12" ,"JetBrainsMono Nerd Font Mono:style:medium:size=12"," ComicCodeLigaturesNerdFontComplete Nerd Font:style=Regular:size=12", "Noto Sans CJK JP:style=Regular:size=12", "Noto Sans JP Medium:style=Mediun:size=12", "FreeMono:style=Regular:size=12" };
 //static const char *fonts[]          = {"FreeMono:style:Regular:size=12"};
 
 // theme
 #include "themes/catppuccin.h"
-/*#include "themes/dracula.h"*/
+//#include "themes/dracula.h"
 
 static const char *colors[][3]      = {
     /*                     fg       bg      border */
@@ -123,8 +123,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    { "[\\]",     dwindle },
     { "><>",      NULL },    /* no layout function means floating behavior */
+    { "[\\]",     dwindle },
     { "[M]",      monocle },
     { "[]=",      tile },    /* first entry is default */
     { "[@]",      spiral },
@@ -175,7 +175,8 @@ static const Key keys[] = {
 
     { MODKEY,                           XK_d,       spawn,          SHCMD("rofi -show drun") },
     { MODKEY,                           XK_w,       spawn,          SHCMD("rofi -modi emoji -show emoji") },
-    { MODKEY,                           XK_Return,  spawn,            SHCMD("st -g 80x24+0+300")},
+    // { MODKEY,                           XK_Return,  spawn,            SHCMD("st -g 80x24+0+300")},
+    { MODKEY,                           XK_Return,  spawn,            SHCMD("ghostty")},
     { MODKEY,                           XK_o,  spawn,            SHCMD("zen-browser")},
     { MODKEY,                           XK_n,  spawn,            SHCMD("thunar")},
     { MODKEY,                           XK_y,  spawn,            SHCMD("clipcat-menu")},
@@ -212,6 +213,7 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,                 XK_k,       movestack,      {.i = -1 } },
     { MODKEY|ShiftMask,                 XK_Return,  zoom,           {0} },
     { MODKEY,                           XK_Tab,     view,           {0} },
+    { Mod1Mask,                         XK_Tab,     spawn,           SHCMD("rofi -show window") },
 
     // overall gaps
     { MODKEY|ControlMask,               XK_i,       incrgaps,       {.i = +1 } },
--- dwm.c
+++ dwm.c
@@ -769,6 +769,7 @@ void clientmessage(XEvent *e) {
   XSetWindowAttributes swa;
   XClientMessageEvent *cme = &e->xclient;
   Client *c = wintoclient(cme->window);
+  unsigned int i;
 
   if (showsystray && cme->window == systray->win &&
       cme->message_type == netatom[NetSystemTrayOP]) {
@@ -838,6 +839,14 @@ void clientmessage(XEvent *e) {
   } else if (cme->message_type == netatom[NetActiveWindow]) {
     if (c != selmon->sel && !c->isurgent)
       seturgent(c, 1);
+		for (i = 0; i < LENGTH(tags) && !((1 << i) & c->tags); i++);
+		if (i < LENGTH(tags)) {
+			const Arg a = {.ui = 1 << i};
+			selmon = c->mon;
+			view(&a);
+			focus(c);
+			restack(selmon);
+		}
   }
 }
 
